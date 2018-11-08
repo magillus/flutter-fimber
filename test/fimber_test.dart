e040b258c46d52fc94833dddc439e3e4c0a92d99
Mateusz Perlak <mati.akip@gmail.com>
2018-11-07 19:42:00 -0600

    fixed unit tests clearing old trees

--- test/fimber_test.dart
+++ test/fimber_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:fimber/fimber.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('STATIC - log DEBUG when filtered out', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W"]);
     Fimber.plantTree(assertTree);
     Fimber.d("Test message", ex: Exception("test error"));
@@ -11,13 +11,16 @@ void main() {
   });
 
   test('STATIC - log DEBUG when expected', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D"]);
+    Fimber.plantTree(DebugTree());
     Fimber.plantTree(assertTree);
     Fimber.d("Test message", ex: Exception("test error"));
     assert(assertTree.lastLogLine != null);
   });
 
   test('STATIC - log DEBUG with exception', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D"]);
     Fimber.plantTree(assertTree);
     Fimber.d("Test message", ex: Exception("test error"));
@@ -26,6 +29,7 @@ void main() {
   });
 
   test('STATIC - log INFO message tag', (){
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W"]);
     Fimber.plantTree(assertTree);
     Fimber.i("Test message");
@@ -42,6 +46,7 @@ void main() {
   });
 
   test('STATIC - log VERBOSE message tag', (){
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     Fimber.v("Test message");
@@ -50,6 +55,7 @@ void main() {
   });
 
   test('STATIC - log ERROR message tag', (){
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     Fimber.e("Test message");
@@ -58,6 +64,7 @@ void main() {
   });
 
   test('STATIC - log WARNING message tag', (){
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     Fimber.w("Test message");
@@ -66,6 +73,7 @@ void main() {
   });
 
   test('TAGGED - log VERBOSE message with exception', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     var logger = FimberLog("MYTAG");
@@ -76,6 +84,7 @@ void main() {
   });
 
   test('TAGGED - log DEBUG message with exception', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     var logger = FimberLog("MYTAG");
@@ -86,6 +95,7 @@ void main() {
   });
 
   test('TAGGED - log INFO message with exception', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     var logger = FimberLog("MYTAG");
@@ -96,6 +106,7 @@ void main() {
   });
 
   test('TAGGED - log WARNING message with exception', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     var logger = FimberLog("MYTAG");
@@ -106,6 +117,7 @@ void main() {
   });
 
   test('TAGGED - log ERROR message with exception', () {
+    Fimber.clearAll();
     var assertTree = AssertTree(["I", "W", "D", "E", "V"]);
     Fimber.plantTree(assertTree);
     var logger = FimberLog("MYTAG");
