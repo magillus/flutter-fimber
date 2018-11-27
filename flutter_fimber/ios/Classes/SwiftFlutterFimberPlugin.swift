a1eddcce402122ba15501380abf5a8ab58c0cf10
Mateusz Perlak <mateusz.perlak@omnigon.com>
2018-11-26 21:09:30 -0600

    iOS handling the log method payload

--- flutter_fimber/ios/Classes/SwiftFlutterFimberPlugin.swift
+++ flutter_fimber/ios/Classes/SwiftFlutterFimberPlugin.swift
@@ -9,6 +9,21 @@ public class SwiftFlutterFimberPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    guard call.method == "log" else {
+        result(FlutterMethodNotImplemented)
+        return
+    }
+    do {
+     
+        let data = call.arguments as! NSDictionary
+        print("level = ", data["level"])//["level"])
+        print("message = ", data["message"])
+        print("tag = ", data["tag"])
+        //print("tag: %s", t0)
+        //    print("level: %s", tag.level)
+        result(0)
+    } catch {
+        result(-1)
+    }
   }
 }
