ce64773a31d6d914691337d70f975f99345afa7b
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-09-29 09:12:19 -0300

    Fix no method channel on Android

--- flutter_fimber/android/src/main/kotlin/com/magillus/flutter_fimber/FlutterFimberPlugin.kt
+++ flutter_fimber/android/src/main/kotlin/com/magillus/flutter_fimber/FlutterFimberPlugin.kt
@@ -1,7 +1,6 @@
 package com.magillus.flutter_fimber
 
-import androidx.annotation.NonNull
-
+import android.util.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -16,20 +15,38 @@ class FlutterFimberPlugin: FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_fimber")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when (call.method) {
+      "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      "log" -> log(call, result)
+      else -> result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  private fun log(call: MethodCall, result: Result) {
+    val tag = call.argument<String?>("tag") ?: "flutter"
+    val message = call.argument<String?>("message") ?: ""
+    val level = call.argument<String?>("level") ?: "V"
+    val throwable = call.argument<String?>("ex")?.let { Throwable(it) }
+
+    when (level) {
+      "V" -> Log.v(tag, message, throwable)
+      "D" -> Log.d(tag, message, throwable)
+      "I" -> Log.i(tag, message, throwable)
+      "W" -> Log.w(tag, message, throwable)
+      "E" -> Log.e(tag, message, throwable)
+      "WTF" -> Log.wtf(tag, message, throwable)
+      else -> Log.d(tag, message, throwable)
+    }
+    result.success(null)
   }
 }
